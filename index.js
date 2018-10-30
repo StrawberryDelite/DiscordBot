33b834a9b5195276e0e4a38bcfe6fc05e09adcad
RED-00 <PSantos211@gmail.com>
2018-10-30 05:10:10 -0700

    Initialize

---
+++ index.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const { token, prefix} = require("./config.json");
+
+
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+    
+    if (message.content.startsWith("the")) {
+        message.channel.send("game!");
+    }
+
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    if (message.content.startsWith(prefix + "ping")) {
+        message.channel.send("pong!");
+    }
+});
+ 
+client.login(token);
